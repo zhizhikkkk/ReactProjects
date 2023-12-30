caab00a9d52f7bedefc4072b476ba84d033287b9
zhizhikkkk <zhanmamyt@bk.ru>
2023-12-31 01:02:23 +0600

    Mapping cards through data

--- FoodApp/food-app/src/App.js
+++ FoodApp/food-app/src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HeadlineCards from "./components/HeadlineCards";
+import Food from "./components/Food";
 
 function App() {
   return (
     <div >
       <Navbar/>
       <Hero/>
-        <HeadlineCards/>
+      <HeadlineCards/>
+      <Food/>
     </div>
   );
 }
---
+++ FoodApp/food-app/src/data/data.js
@@ -0,0 +1,122 @@
+export const data =[
+    {
+        id:1,
+        name: 'Strawberry',
+        category:'berries',
+        image:'https://images.pexels.com/photos/89778/strawberries-frisch-ripe-sweet-89778.jpeg?auto=compress&cs=tinysrgb&w=600',
+        price:'5000'
+    },
+    {
+        id:2,
+        name: 'Grape',
+        category:'berries',
+        image:'https://images.pexels.com/photos/760281/pexels-photo-760281.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        price:'3000'
+    },
+    {
+        id:3,
+        name: 'Banan',
+        category:'fruits',
+        image:'https://images.pexels.com/photos/5966630/pexels-photo-5966630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        price:'2000'
+    },
+    {
+        id:4,
+        name: 'Orange',
+        category:'citrus',
+        image:'https://images.pexels.com/photos/2090899/pexels-photo-2090899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        price:'3500'
+    },
+    {
+        id:5,
+        name: 'Mango',
+        category:'exotic',
+        image:'https://images.pexels.com/photos/4023132/pexels-photo-4023132.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        price:'4200'
+    },
+    {
+        id:6,
+        name: 'Pineapple',
+        category:'fruits',
+        image:'https://images.pexels.com/photos/1161547/pexels-photo-1161547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        price:'7000'
+    },
+    {
+        id:7,
+        name: 'Apple',
+        category:'fruits',
+        image:'https://images.pexels.com/photos/209439/pexels-photo-209439.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        price:'2100'
+    },
+    {
+        id:8,
+        name: 'Watermelon',
+        category:'fruits',
+        image:'https://images.pexels.com/photos/2288692/pexels-photo-2288692.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        price:'3500'
+    },
+    {
+        id:9,
+        name: 'Bleuberry',
+        category:'berries',
+        image:'https://images.pexels.com/photos/1207288/pexels-photo-1207288.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        price:'10000'
+    },
+    {
+        id:10,
+        name: 'Kiwi',
+        category:'fruits',
+        image:'https://images.pexels.com/photos/867349/pexels-photo-867349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        price:'4500'
+    },
+    {
+        id:11,
+        name: 'Pomegranate',
+        category:'fruits',
+        image:'https://images.pexels.com/photos/65256/pomegranate-open-cores-fruit-fruit-logistica-65256.jpeg?auto=compress&cs=tinysrgb&w=600',
+        price:'5000'
+    },
+    {
+        id:12,
+        name: 'Lime',
+        category:'citrus',
+        image:'https://images.pexels.com/photos/4590488/pexels-photo-4590488.jpeg?auto=compress&cs=tinysrgb&w=600',
+        price:'3000'
+    },
+    {
+        id:13,
+        name: 'Limon',
+        category:'citrus',
+        image:'https://images.pexels.com/photos/952360/pexels-photo-952360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        price:'2000'
+    },
+    {
+        id:14,
+        name: 'Tangerine',
+        category:'berries',
+        image:'https://images.pexels.com/photos/2294477/pexels-photo-2294477.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        price:'2000'
+    },
+    {
+        id:15,
+        name: 'Mangosteen',
+        category:'exotic',
+        image:'https://images.pexels.com/photos/2132031/pexels-photo-2132031.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        price:'12000'
+    },
+    {
+        id:16,
+        name: 'Pitahaya',
+        category:'exotic',
+        image:'https://images.pexels.com/photos/5232860/pexels-photo-5232860.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        price:'2600'
+    },
+    {
+        id:17,
+        name: 'Karambola',
+        category:'exotic',
+        image:'https://media.istockphoto.com/id/518225859/photo/star-fruit.jpg?s=2048x2048&w=is&k=20&c=HUJuPQjv4Jrpu8zJBwpuo8_8K0ca555QtaA4ohTEHwI=',
+        price:'5800'
+    }
+
+]
